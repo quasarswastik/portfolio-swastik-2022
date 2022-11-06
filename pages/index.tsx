b09127c88dffb03be791680ca2f80ed7623fe7c5
quasarswastik <quasarswastik@gmail.com>
2022-11-06 16:25:27 +0800

    Created the header, the next phase coming up Added additional files libraries for motion

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,6 @@
 import Head from 'next/head'
+import Header from '../components/Header'
+import Hero from '../components/Hero'
 
 export default function Home() {
   return (
@@ -7,7 +9,23 @@ export default function Home() {
         <title>Swastik Raj Chauhan</title>
       </Head>
 
-      <h1 className="p-10 text-red-500">Portfolio 2022</h1>
+      {/* Header */}
+      <Header />
+
+      {/* {Hero} */}
+      <section id="hero">
+        <Hero />
+      </section>
+
+      {/* {About} */}
+
+      {/* {Experience} */}
+
+      {/* {Skills} */}
+
+      {/* {Projects} */}
+
+      {/* {Contact Me} */}
     </div>
   )
 }
