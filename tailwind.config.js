58b565c2c9923b3f60d4dbe9c4d852badc2ee025
quasarswastik <quasarswastik@gmail.com>
2022-11-06 13:30:40 +0800

    Tailwind css set up

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  // this tells tailwind css where to apply the css at
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
